6efde395042427d4e111e67c76acba0530b94794
Peggy Fernandez <peggy.fernandez-blanco@univ-amu.fr>
2023-04-27 15:40:32 +0200

    Correction coquille

--- src/Controller/GroupController.php
+++ src/Controller/GroupController.php
@@ -1829,7 +1829,7 @@ class GroupController extends AbstractController {
                         'prefixe' => $i_default,
                         'nom' => $nom,
                         'desc' => $desc,
-                        'filter' => $filtAff));
+                        'filter' => $filtreAff));
 
                 $form->handleRequest($request);
                 if ($form->isSubmitted() && $form->isValid()) {
